afff7f77955dcacd2d1135c0ccb971f8ff8fe015
Mikael Berglund <mikael.berglund@tele2.com>
2024-01-22 16:40:56 +0100

    Added Readme etc.

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 import http from 'k6/http';
+import exec from 'k6/execution';
 import { check, sleep } from 'k6';
 
 export const options = {
   executor: 'ramping-arrival-rate',
+  thresholds: {
+    http_req_failed: [{
+      threshold: 'rate<0.000001',
+      abortOnFail: true,
+    }], 
+  },
   stages: [
-    { duration: '1m', target: 5000 },
+    { duration: '2m', target: 20000 },
   ],
 };
 
 export default function() {
-  const res = http.get('http://localhost:8080/?start=0&end=10000');
+  const res = http.get('http://18.196.63.38/?start=0&end=10000');
   const nr = res.json().random_number;
   check(nr, {
     'is in range': (s) => nr < 10000 && nr >= 0
